6141839fdfc21f838e9827e97d69d03e85f28be4
Taavi Toomsalu <queenshelob@mail.com>
2023-07-12 11:44:44 +0300

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Grid } from '@mui/material';
+
+import { SearchBar, VideoDetail } from './components/';
+
+import youtube from './api/youtube';
+
+class App extends React.Component {
+    state = {
+        video: [],
+        selectedVideo: null,
+    }
+    handleSubmit = async (searchTerm) => {
+        try {
+            const response = await youtube.get('search', { 
+                params: {
+                    part: 'snippet',
+                    maxResults: '5',
+                    key: 'AIzaSyAwePyRcd4FIphWlv00OAZ2EQiZJxtf2m8',
+                    q: searchTerm
+                }
+            })
+
+            this.setState({ videos: response.data.items, selectedVideo: response.data.items[0]})
+
+        } catch (error) {
+            console.error('Error occurred:', error);
+        }
+    }
+
+    render () {
+        return (
+            <Grid justify="center" container spacing={10}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={12}>
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetail/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* VIDEO LIST */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        )
+    }
 }
+
+
+
+
 
 export default App;
